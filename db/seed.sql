024ae80460590b6534fb6abfc1b24bc449175743
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-04-26 20:43:46 -0500

    inquirer functionality and can add roles, employees, roles

---
+++ db/seed.sql
@@ -0,0 +1,28 @@
+USE e_tracker;
+
+CREATE TABLE department (
+iddep INTEGER NOT NULL AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (iddep)
+);
+
+USE e_tracker;
+CREATE TABLE role (
+idrole INTEGER NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL (8,2) NOT NULL,
+iddep INTEGER,
+PRIMARY KEY (idrole),
+FOREIGN KEY (iddep) REFERENCES department (iddep)
+)ENGINE=INNODB;
+
+USE e_tracker;
+CREATE TABLE employee (
+idem INTEGER NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_first VARCHAR(30) NOT NULL,
+idrole INTEGER,
+manager_id INTEGER NULL,
+PRIMARY KEY (idem),
+FOREIGN KEY (idrole) REFERENCES role (idrole)
+)ENGINE=INNODB;
--- employee.js
+++ employee.js
@@ -15,3 +15,229 @@ const connection = mysql.createConnection({
   password: 'Mons@li17',
   database: 'e_tracker',
 });
+
+let rolesArray = []; //how to make this dynamic
+let depArray = [];
+let managerArray = [];
+class Department {
+    constructor(id, name) {
+      this.name = name;
+      this.value = id;
+    }
+};
+
+class Role {
+    constructor(id, name){
+        this.name = name;
+        this.value = id;
+    }
+}
+
+class Manager {
+    constructor(id, name){
+        this.name = name;
+        this.value = id;
+    }
+}
+
+const newdep = () => {
+    connection.query (
+        'SELECT * FROM department', (err, res) => {
+            if(err) throw err;
+            for (i = 0 ; i < res.length; i++) {
+                const temp = new Department (res[i].iddep, res[i].name)
+                depArray.push(temp)
+            }
+        }
+    )
+}
+
+const newrole = () => {
+    connection.query(
+        'SELECT * FROM role', (err, res) => {
+            if (err) throw err;
+            for(i = 0; i < res.length; i++) {
+                const temp = new Role (res[i].idrole, res[i].title)
+                rolesArray.push(temp);
+            }
+        }
+    )
+}
+
+const newmanager = () => {
+    connection.query("SELECT employee.first_name, employee.last_first, employee.idem FROM employee INNER JOIN role ON role.idrole = employee.idrole WHERE role.title=?",['Manager'],
+     (err, res) => {
+        if (err) throw err;
+        const tempA = res.map((res) => `${res.first_name} ${res.last_first}`);
+        for(i = 0;i <res.length; i++) {
+            const temp = new Manager (res[i].idem, tempA[i])
+            managerArray.push(temp);
+        }
+        const temp = new Manager (0, 'None')
+        managerArray.push(temp);
+    })
+}
+
+const start = () => {
+    inquirer
+    .prompt({
+        name: 'mainquestion',
+        type: 'list',
+        message: 'What function would you like to perform?',
+        choices: ['Add Department', 'Add Roles', 'Add Employees', 'View Departments', 'View Roles', 'View Employees', "Update Employee Role", 'Done'],
+    })
+    .then((answer) => {
+        if (answer.mainquestion === 'Add Department') {
+            departmentQuestions();
+        } else if (answer.mainquestion === 'Add Employees') {
+            employeeQuestions();
+        } else if (answer.mainquestion === 'Add Roles') {
+            roleQuestions();
+        } else if (answer.mainquestion === 'View Departments') {
+            viewDepartments();
+        } else if (answer.mainquestion === 'View Roles') {
+            viewRoles();
+        } else if (answer.mainquestion === 'View Employees') {
+            viewEmployees();
+        } else if (answer.mainquestion === 'Update Employee Role'){
+            updateEmployeeRoles();
+        } else {
+            connection.end();
+        }
+    })
+}
+
+const departmentQuestions = () => {
+    inquirer
+    .prompt({
+        name: 'name',
+        type: 'input',
+        message: 'What is the department name you would like to add?'
+    })
+    .then((response) => {
+        connection.query(
+            'INSERT INTO department SET ?',
+            {
+              name: response.name,  
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Your Department has succesfully been created!');
+                newdep()
+                start();
+            }
+        )
+    })
+}
+
+const employeeQuestions = () => {
+    inquirer
+    .prompt([
+    {
+        name: 'fname',
+        type: 'input',
+        message: "What is the employee's first name?"
+    },
+    {
+        name: 'lname',
+        type: 'input',
+        message: "What is the employee's last name?"
+    },
+    {
+        name: "role",
+        type: 'list',
+        message: "What is the employee's role?",
+        choices: rolesArray,
+    },
+    {
+        name: 'manager',
+        type: 'list',
+        message: "Who is the employee's manager?",
+        choices: managerArray,
+    },
+    ])
+    .then((response) => {
+        if (response.manager === 0) {
+            connection.query(
+                "INSERT INTO employee SET ?",
+                {
+                    first_name: response.fname,
+                    last_first: response.lname,
+                    idrole: response.role,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Your Role has been succesfully added!');
+                    newmanager();
+                    start();
+                }
+            )
+        } else (
+        connection.query(
+            "INSERT INTO employee SET ?",
+             {
+                first_name: response.fname,
+                last_first: response.lname,
+                idrole: response.role,
+                manager_id: response.manager,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Your Role has been succesfully added!');
+                start();
+            }
+        ))
+     })
+}
+
+const roleQuestions = () => {
+    inquirer
+    .prompt([
+    {
+        name: 'name',
+        type: 'input',
+        message: 'What is the name of the role you would like to add?',
+    },
+    {
+        name: 'salary',
+        type: 'input',
+        message: 'What is the salary of the role?',
+        validate(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          },
+    },
+    {
+        name: 'department',
+        type: 'list',
+        message: 'In what department is this role?',
+        choices: depArray,
+    },
+    ])
+    .then((response) => {
+        connection.query(
+            "INSERT INTO role SET ?",
+            {
+                title: response.name,
+                salary: response.salary,
+                iddep: response.department,
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Your Role has been succesfully added!');
+                newrole();
+                start();
+            }
+        )
+    })
+}
+
+connection.connect((err) => {
+    if (err) throw err;
+    start()
+    newdep();
+    newrole();
+    newmanager();
+})
